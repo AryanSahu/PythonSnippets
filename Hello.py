e4bcd096bf8f00967b3cd9609c5047dddc5d5e7d
Ganesh Sahu <ganesh.sahu@imaginea.com>
2019-11-06 20:52:37 +0530

    Python Age calculator

---
+++ Hello.py
@@ -0,0 +1,9 @@
+patientName='John Smith'
+age=20
+isNewPatient=True;
+
+patientName=input("Enter your name  ")
+print("Hi "+patientName)
+year=input("Enter birthday Year  ")
+age=2019-int(year)
+print(age)
