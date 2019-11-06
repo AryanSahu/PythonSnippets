3d673a09423d636366bc37526f8550f8c22384f3
Ganesh Sahu <ganesh.sahu@imaginea.com>
2019-11-06 21:28:37 +0530

    String format example

--- StringManipulation.py
+++ StringManipulation.py
@@ -35,3 +35,14 @@ print(a.split(",")) # returns ['Hello', ' World!']
 txt = "The rain in Spain stays mainly in the plain"
 x = "ain" in txt
 print(x)
+
+
+age = 36
+txt = "My name is John, and I am  {}"
+print(txt.format(age));
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
