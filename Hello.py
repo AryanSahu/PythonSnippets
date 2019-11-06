4f14e14f51625a2bd82c11db3d481748ce804b45
Ganesh Sahu <ganesh.sahu@imaginea.com>
2019-11-06 22:07:54 +0530

    Math import

--- Hello.py
+++ Hello.py
@@ -1,7 +1,12 @@
+import math
+
+
 patientName='John Smith'
 age=20
 isNewPatient=True;
 
+h_name=math.floor(12.9)
+print(h_name)
 patientName=input("Enter your name  ")
 print("Hi "+patientName)
 year=input("Enter birthday Year  ")
